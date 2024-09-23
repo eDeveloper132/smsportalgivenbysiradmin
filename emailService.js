be8509a31fc7770c0eed0b301ca51298a0a089ae
eDeveloper132 <1375219.aptechiic@gmail.com>
2024-09-23 14:44:30 +0500

    second

--- emailService.js
+++ emailService.js
@@ -6,7 +6,7 @@ async function sendVerificationEmail(Email, verificationToken) {
         console.error("No recipient email defined");
         return;
     }
-    const verificationURL = new URL(`https://smsportalwpcodersforadmin-three.vercel.app/verify-email`);
+    const verificationURL = new URL(`https://smsportalgivenbysiradmin.vercel.app/verify-email`);
     verificationURL.searchParams.append('token', verificationToken);
     const mailOptions = {
         from: process.env.EMAIL_USER,
